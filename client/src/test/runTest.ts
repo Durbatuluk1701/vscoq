01aed48a9b08bb060309b873b4cf156b5dd15edd
Will Thomas <30wthomas@gmail.com>
2025-02-19 14:34:46 -0600

    changing to use specific vscode versions

--- client/src/test/runTest.ts
+++ client/src/test/runTest.ts
@@ -38,9 +38,9 @@ async function main() {
 		const launchArgs = [path.resolve(__dirname, '../../testFixture'), "--disable-extensions", "--user-data-dir=" + userDataDir];
 
 		// Download VS Code, unzip it and run the integration test
-        //const vscodeExecutablePath = await downloadAndUnzipVSCode('1.84.0');
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.96.4');
 		await runTests({ 
-            // vscodeExecutablePath, 
+            vscodeExecutablePath, 
             extensionDevelopmentPath, 
             extensionTestsPath, 
             launchArgs });
