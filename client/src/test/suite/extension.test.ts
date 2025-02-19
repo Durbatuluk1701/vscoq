520b9474a7e77fbfc05d0a20966e01d41b929195
Will Thomas <30wthomas@gmail.com>
2025-02-19 12:26:06 -0600

    Temporary CI updates and print debug

--- client/src/test/suite/extension.test.ts
+++ client/src/test/suite/extension.test.ts
@@ -9,24 +9,35 @@ suite('Should get diagnostics', function () {
 	this.timeout(30000);
 
 	test('Diagnoses an undefined ref error', async () => {
+    console.log('1');
 
 		const ext = vscode.extensions.getExtension('maximedenes.vscoq')!;
+    console.log('2');
     const doc = await common.openTextFile('basic.v');
+    console.log('3');
 
 		await ext.activate();
+    console.log('4');
 		vscode.workspace.getConfiguration().update('vscoq.proof.mode','Continuous');
+    console.log('5');
 
 		await common.sleep(10000); // Wait for server initialization
+    console.log('6');
 
 		const diagnostics = vscode.languages.getDiagnostics(doc);
+    console.log('7');
 		
 		expect(diagnostics.length).toBe(1);
+    console.log('8');
 
 		const diagnostic = diagnostics[0];
+    console.log('9');
 
 		expect(diagnostic.message).toMatch(/The reference zar was not found.*/);
+    console.log('10');
 		
 		expect(diagnostic.severity).toBe(vscode.DiagnosticSeverity.Error);
+    console.log('11');
 	
 	});
 
